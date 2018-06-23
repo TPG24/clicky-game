53253a63c5d8a2f20647fafe9b06812023e0d12f
Your Name <tonygiamboy@yahoo.com>
2018-06-23 17:32:25 -0400

    changed in json for deployment on github

--- clickclick/src/components/navbar.js
+++ clickclick/src/components/navbar.js
@@ -22,7 +22,7 @@ const styles = {
 const Nav = props => (
     <nav className="navbar navbar-expand-lg sticky-top" style={styles.navbar}>
         <h1 className="navbar-brand" style={styles.logo} href="#"><span>{props.brand}</span></h1>
-        
+
         <div className="scoreboard ml-auto d-flex">
             <h4>User Score: <span className="p-2" id="userScore" style={styles.scoreBoard}>{props.userScore}</span></h4>
             <h4>Top Score: <span className="p-2" id="topScore" style={styles.scoreBoard}>{props.topScore}</span></h4>
