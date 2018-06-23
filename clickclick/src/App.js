8e750ecdb731edb9253bc1bbe9802e0d32bef573
Your Name <tonygiamboy@yahoo.com>
2018-06-23 00:36:47 -0400

    image cards random, css applied, google font applied, all images added

--- clickclick/src/App.js
+++ clickclick/src/App.js
@@ -1,21 +1,83 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Nav from "./components/navbar";
+import Header from "./components/header";
+import Main from "./components/main";
+import Footer from "./components/footer";
+import GameCard from "./components/GameCard";
+import cardData from "./cardData.json";
+
+function shuffleArray(array) {
+    let i = array.length - 1;
+    for (; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        const temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+    return array;
+}
+
+const correct = ["YYYYYEAHHHHH!", "So Fresh! So Clean!", "DIRTY SOUTH", "GUCCI"];
+const incorrect = ["Never Forget Atlanta!!", "TO THE WINDOW, TO THE YOU LOSE!", "Start Over"];
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+
+    state = {
+        brand: "Atlanta-Click-It-To-Win-It!",
+        userSelect: '',
+        userScore: 0,
+        topScore: 0
+    };
+
+
+    render() {
+        const shuffledCards = shuffleArray(cardData);
+        const randCorrect = correct[Math.floor((Math.random() * (correct.length -1)) + 1)];
+        const randIncorrect = incorrect[Math.floor((Math.random() * (incorrect.length -1)) + 1)];
+
+        return (
+            <div className="App">
+                <Nav userScore={this.state.userScore} topScore={this.state.topScore} brand={this.state.brand}/>
+                <Header />
+                <Main>
+                    {shuffledCards.map(card =>
+                        <GameCard key={card.id}
+                                  {...card}
+                                  selectImage={(id) => {
+                                      // Set current state for selected image Id
+                                      this.setState({userSelect: id});
+
+                                      // JS function for game play and updating user score
+                                      this.setState((prevState) => {
+                                          if (this.state.userSelect === prevState.userSelect) {
+                                              return ({
+                                                  userScore : 0,
+                                                  brand: randIncorrect
+                                              })
+                                          } else {
+                                              if (this.state.topScore <= this.state.userScore) {
+                                                  return ({
+                                                      userScore : this.state.userScore + 1,
+                                                      topScore : this.state.topScore + 1,
+                                                      brand: randCorrect
+                                                  })
+                                              } else if (this.state.topScore >= this.state.userScore) {
+                                                  return ({
+                                                      userScore : this.state.userScore + 1,
+                                                      brand: randCorrect
+                                                  })
+                                              }
+                                          }
+                                      })
+                                  }}
+                        />
+                    )}
+                </Main>
+                <Footer />
+            </div>
+        );
+    }
 }
 
 export default App;
---
+++ clickclick/src/components/footer.js
@@ -0,0 +1,19 @@
+
+import React from "react";
+
+const styles = {
+    footer: {
+        background: "#202020",
+        color: "#ffffff",
+    }
+};
+
+const Footer = () => (
+    <footer className="footer d-center p-2" style={styles.footer}>
+        <div className="bottom">
+            Atlanta-Click-It-To-Win-It!
+        </div>
+    </footer>
+);
+
+export default Footer;
